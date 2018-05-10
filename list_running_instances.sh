c6f4400c069632eaf24a4feb44c7f91825bb5910
Peter Bryzgalov <peterbryz@yahoo.com>
2018-05-10 15:47:07 +0900

    Removed sourcing .profile, using absolute path to jq instead.

--- list_running_instances.sh
+++ list_running_instances.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-. ~/.profile
 echo "US"
-/usr/local/bin/aws ec2 describe-instances --profile us --filters "Name=instance-state-name,Values=running" --output json | jq -c -r ".Reservations[] | .Instances[] | [.InstanceType, .State.Name, .PublicDnsName] | @csv " | sed 's/\"//g' | sed "s/,/ /g"
+/usr/local/bin/aws ec2 describe-instances --profile us --filters "Name=instance-state-name,Values=running" --output json | /usr/local/bin/jq -c -r ".Reservations[] | .Instances[] | [.InstanceType, .State.Name, .PublicDnsName] | @csv " | sed 's/\"//g' | sed "s/,/ /g"
 echo "US-Eeast-2"
-/usr/local/bin/aws ec2 describe-instances --profile us-e2 --filters "Name=instance-state-name,Values=running" --output json | jq -c -r ".Reservations[] | .Instances[] | [.InstanceType, .State.Name, .PublicDnsName] | @csv " | sed 's/\"//g' | sed "s/,/ /g"
+/usr/local/bin/aws ec2 describe-instances --profile us-e2 --filters "Name=instance-state-name,Values=running" --output json | /usr/local/bin/jq -c -r ".Reservations[] | .Instances[] | [.InstanceType, .State.Name, .PublicDnsName] | @csv " | sed 's/\"//g' | sed "s/,/ /g"
 echo "JP"
-/usr/local/bin/aws ec2 describe-instances --profile jp --filters "Name=instance-state-name,Values=running" --output json | jq -c -r ".Reservations[] | .Instances[] | [.InstanceType, .State.Name, .PublicDnsName] | @csv " | sed 's/\"//g' | sed "s/,/ /g"
+/usr/local/bin/aws ec2 describe-instances --profile jp --filters "Name=instance-state-name,Values=running" --output json | /usr/local/bin/jq -c -r ".Reservations[] | .Instances[] | [.InstanceType, .State.Name, .PublicDnsName] | @csv " | sed 's/\"//g' | sed "s/,/ /g"
